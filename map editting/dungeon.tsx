651d483ff789d020caa8859647aae26b01e2cc7c
pineapplezz <idontknowwhattoputt@gmail.com>
2019-09-13 14:31:45 -0400

    resourced image location

--- map editting/dungeon.tsx
+++ map editting/dungeon.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="dungeon" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../Downloads/tileset_dungeon.png" width="256" height="256"/>
+ <image source="../assets/tileset_dungeon.png" width="256" height="256"/>
 </tileset>
